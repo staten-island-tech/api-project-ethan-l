5a593f531b24cd202456d1b5cb434c365faf6daa
Ehan <ethanliang405@gmail.com>
2021-05-09 20:05:36 -0400

    info for one of the cards

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -13,6 +13,16 @@ const DOMSelectors = {
   seeAllTopUpcoming: document.getElementById("see-all-top-upcoming"),
   seeAllHighestRated: document.getElementById("see-all-highest-rated"),
   seeAllMostPopular: document.getElementById("see-all-most-popular"),
+
+  infoPage: document.getElementById("info-page"),
+  infoCard: document.getElementById("info-card"),
+  leftPanel: document.getElementById("info-left-panel"),
+  infoTitle: document.getElementById("info-title"),
+  infoSynopsis: document.getElementById("info-synopsis"),
+  infoSongs: document.getElementById("info-songs"),
+  flexGrowOne: document.getElementById("flex-grow-1"),
+
+  cards: document.getElementsByClassName("card"),
 };
 
 export { DOMSelectors };
--- src/js/index.js
+++ src/js/index.js
@@ -16,7 +16,7 @@ const topSeasonals = async function () {
       if (anime.episodes === 0) {
         DOMSelectors.topSeasonals.insertAdjacentHTML(
           "beforeend",
-          `<div class="card">
+          `<div class="card" data-id="${anime.mal_id}">
           <p class="card-rating">${anime.score}/10</p>
           <img class="card-img" src="${anime.image_url}" alt="">
           <div class="card-text">
@@ -316,7 +316,120 @@ const seeAllMostPopular = function () {
 };
 seeAllMostPopular();
 
-//Do the same for each section
-//One single info page generic for each anime
+const openInfo = function () {
+  console.log(DOMSelectors.cards);
+  let cardsArray = Array.from(DOMSelectors.cards);
+  console.log(cardsArray);
 
-//jikan data is different from mal data?
+  cardsArray[0].addEventListener("click", async function () {
+    console.log(cardsArray[0].dataset.id);
+    //`https://api.jikan.moe/v3/anime/${cardsArray[0].dataset.id}`
+    try {
+      const response = await fetch(
+        `https://api.jikan.moe/v3/anime/${cardsArray[0].dataset.id}`
+        //watch whalen video to see how he did it
+      );
+      const data = await response.json();
+      console.log(data);
+
+      document.body.insertAdjacentHTML(
+        "beforeend",
+        `    <div class="info-page-background" id="info-page">
+    
+        <div class="card-info" id="info-card">
+    
+          <div class="info-left-panel" id="info-left-panel">
+            <img src="${data.image_url}" alt="${data.title} Cover">
+            <p class="info-episodes">Episodes: ${data.episodes}</p>
+            <p class="info-status">Status: ${data.status}</p>
+            <p class="info-air-date">Aired: ${data.aired.string}</p>
+            <p class="info-duration">Length: ${data.duration} (${data.type})</p>
+            <p class="info-rating">${data.rating}</p>
+    
+            <p class="info-genre">Genres: ${data.genres[0].name}</p>
+    
+            <p class="info-studios">Studio: ${data.studios[0].name}</p>
+    
+            <p class="info-score">Score: ${data.score}/10</p>
+            <p class="info-popularity-rank">Popularity: ${data.members} (#${data.popularity})</p>
+            <p class="info-rank">Rank: ${data.rank}</p>
+    
+            <p class="info-source">Source: ${data.source}</p>
+    
+            <a href="${data.url}" class="info-mal-link">Link to MAL</a>
+          </div>
+    
+          <div class="info-right-panel">
+            <h2 class="info-title" id="info-title">${data.title}</h2>
+            <p class="info-synopsis" id="info-synopsis">${data.synopsis}</p>
+            <span class="flex-grow-1" id="flex-grow-1"></span>
+    
+            <div class="info-songs" id="info-songs">
+              <p class="info-openings">Opening themes: ${data.opening_themes}</p>
+              <p class="info-endings">Ending themes: ${data.ending_themes}</p>
+            </div>
+          </div>
+    
+          <!--related, adaptation, sequal, side story-->
+    
+        </div>
+      </div>`
+      );
+      const infoPage = document.getElementById("info-page");
+
+      infoPage.style.opacity = "1";
+      console.log("info opened");
+
+      //Close
+      const closeInfo = function () {
+        document.addEventListener("click", (evt) => {
+          const infoCard = DOMSelectors.infoCard;
+          const leftPanel = DOMSelectors.leftPanel;
+          const infoTitle = DOMSelectors.infoTitle;
+          const infoSynopsis = DOMSelectors.infoSynopsis;
+          const infoSongs = DOMSelectors.infoSongs;
+
+          console.log(infoPage);
+          //const flexGrowOne = DOMSelectors.flexGrowOne;
+
+          let targetElement = evt.target; // clicked element
+
+          do {
+            if (
+              targetElement == leftPanel ||
+              targetElement == infoTitle ||
+              targetElement == infoSynopsis ||
+              targetElement == infoSongs
+            ) {
+              // This is a click inside. Do nothing, just return.
+              return;
+            }
+            // Go up the DOM
+            targetElement = targetElement.parentNode;
+          } while (targetElement);
+
+          // This is a click outside.
+          infoPage.style.opacity = "0";
+          setTimeout(() => {
+            infoPage.parentNode.removeChild(infoPage);
+          }, 501);
+
+          console.log("Closed");
+          //document.getElementById("flyout-debug").textContent = "Clicked outside!";
+        });
+      };
+      closeInfo();
+    } catch (error) {
+      console.log(error);
+      alert("Fetch failed for title.");
+    }
+  });
+  /*   cardsArray.forEach(function () {
+    addEventListener("click", function () {
+      console.log("Clicked on a card");
+    });
+  }); */
+};
+setTimeout(() => {
+  openInfo();
+}, 1000);
